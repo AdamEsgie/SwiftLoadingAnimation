a1ddb16b9da6b37323120e91157db942e994d7e5
AdamEsgie <adamesgie@gmail.com>
2014-11-06 11:27:13 -0500

    ready for upload

---
+++ LoadingAnimationSwift/LoadingView.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingView.swift
+//  LoadingAnimationSwift
+//
+//  Created by Adam Salvitti-Gucwa on 10/20/14.
+//  Copyright (c) 2014 esgie. All rights reserved.
+//
+
+import Foundation
